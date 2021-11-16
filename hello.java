9c6678ed499a361a8b4242021b404e435cb1b9a2
chltjddnjs <93483726+chltjddnjs@users.noreply.github.com>
2021-11-16 13:11:11 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println('Hello java");
 		System.out.println("Github modify Hello java");
+		System.out.println("Git pull");
 	}
 }
 
