4e19ed60ec11468c9294ab24aac6dc2e835ab221
chltjddnjs <csw991125@naver.com>
2021-11-16 12:01:23 +0900

    modify

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println('Hello java");
-		System.out.println("Local modify Hello java");
+		System.out.println("local modify Hello java");
 	}
 }
 
